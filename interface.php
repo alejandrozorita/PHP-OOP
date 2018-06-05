1bfb04c20dd7b492cf003164fd83b25d35b49c4c
Alejandro Zorita <alejandro.zorita@medssocial.com>
2018-06-05 21:06:27 +0200

    Clases Abstract

--- interface.php
+++ interface.php
@@ -1,3 +1,43 @@
 <?php
 
-echo 'Hello!';
+abstract class Unit
+{
+    protected $alive = true;
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function move($direction)
+    {
+        echo "<p>{$this->name} avanza en dirección $direction</p>";
+    }
+
+    abstract public function attack($opponent);
+}
+
+class Soldier extends Unit{
+
+    public function attack($opponent)
+    {
+        echo "<p>{$this->name} ataca con espada $opponent</p>";
+    }
+
+}
+
+class Archer extends Unit{
+
+    public function attack($opponent)
+    {
+        echo "<p>{$this->name} ataca con flecha $opponent</p>";
+    }
+
+}
+
+$unidad = new Archer('Alejandro');
+
+$unidad->move('left');
+
+$unidad->attack('contrario');
