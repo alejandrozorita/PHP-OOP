9658f03cdb99785712c06290158964eb05d4c5f9
Alejandro Zorita <alejandro.zorita@medssocial.com>
2018-07-29 17:55:10 +0200

    autoload

--- interface.php
+++ interface.php
@@ -2,18 +2,12 @@
 
 require 'src/helpers.php';
 
-require 'src/Unit.php';
-require 'src/Soldier.php';
-require 'src/Archer.php';
-require 'src/Armor.php';
-require 'src/BronceArmor.php';
-require 'src/SilverArmor.php';
-require 'src/CursedArmor.php';
 
 
 
-
-
+spl_autoload_register(function ($clasName) {
+    require "src/$clasName.php";
+});
 
 
 $soldier = new Soldier('Soldado');
